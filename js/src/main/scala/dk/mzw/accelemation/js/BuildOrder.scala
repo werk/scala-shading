1234a749acfb8200bb3d0b628d8053adec06afc4
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2015-09-23 20:33:00 +0200

    A parser for the imaginary .ini file format

--- js/src/main/scala/dk/mzw/accelemation/js/BuildOrder.scala
+++ js/src/main/scala/dk/mzw/accelemation/js/BuildOrder.scala
@@ -2,6 +2,8 @@ package dk.mzw.accelemation.js
 
 import dk.mzw.accelemation.js.BuildOrder._
 
+import scala.collection.mutable.ListBuffer
+
 case class BuildOrder(animationId : Id, actions : Seq[Action])
 
 object BuildOrder {
@@ -21,5 +23,36 @@ object BuildOrder {
 
     }
 
+    def show(buildOrder : Action) : String = {
+
+    }
+
+    def readEntries(text : String) : Seq[Entry] = {
+        val entries = new ListBuffer[Entry]()
+        val keyValues = new ListBuffer[(String, String)]()
+        var entryName : Option[String] = None
+        for((line, lineNumber) <- text.lines.map(_.trim).zipWithIndex if !line.isEmpty) {
+            if(line.startsWith("[")) {
+                for(name <- entryName) {
+                    entries += Entry(name, keyValues.toList)
+                    keyValues.clear()
+                }
+                if(!line.endsWith("]")) throw new ParseException("Expected ']'", line, lineNumber + 1)
+                entryName = Some(line.tail.dropRight(1).trim)
+            } else if(entryName.isEmpty) {
+                throw new ParseException("Expected '['", line, lineNumber + 1)
+            } else {
+                line.split("=", 2) match {
+                    case Array(key, value) => keyValues += (key -> value)
+                    case _ => throw new ParseException("Expected '='", line, lineNumber + 1)
+                }
+            }
+        }
+        entries
+    }
+
+    case class ParseException(message : String, line : String, lineNumber : Int) extends RuntimeException(message + " at line " + lineNumber + ":" + line)
+
+    case class Entry(text : String, keyValues : Seq[(String, String)])
 
 }
