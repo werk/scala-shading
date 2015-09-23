6b63754b1205dbce4e6ba59f41d4eea65c5826d3
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2015-09-23 21:02:44 +0200

    Parser for the imaginary .ini format

--- js/src/main/scala/dk/mzw/accelemation/js/BuildOrder.scala
+++ js/src/main/scala/dk/mzw/accelemation/js/BuildOrder.scala
@@ -14,19 +14,51 @@ object BuildOrder {
     case class Effect(factor : Double, effectId : Id) extends Action
     case class Combine(animationId : Id, effectId : Id, flipped : Boolean) extends Action
 
-
     def show(buildOrder : BuildOrder) : String = {
-        val actions = buildOrder.actions.toList.flatMap(show)
-        ("[initial]" :: buildOrder.animationId :: actions).map(_ + "\n").mkString
+        show(Entry("initial", Seq("animationId" -> show(buildOrder.animationId)))) + buildOrder.actions.map(show).mkString
     }
 
-    def show(action : Action) : List[String] = action match {
-        case Effect(factor, effectId) => List("[effect]", s"factor = $factor", s"effectId = $effectId")
-        case Combine(animationId, effectId, flipped) => List("[combine]", s"animationId = $animationId", s"flipped = $flipped")
+    def show(action : Action) : String = action match {
+        case Effect(factor, effectId) => show(Entry("effect", Seq("factor" -> factor.toString, "effectId" -> show(effectId))))
+        case Combine(animationId, effectId, flipped) => show(Entry("combine", Seq("animationId" -> show(animationId), "effectId" -> show(effectId), "flipped" -> flipped.toString)))
     }
 
-    def show(buildOrder : Action) : String = {
+    def show(id : Id) = id.userId + "/" + id.name
 
+    def show(entry : Entry) : String = {
+        "[" + entry.text + "]\n" +
+        entry.keyValues.map { case (k, v) =>
+            k.replaceAll("[\\r\\n]+", " ") + " = " + v.replaceAll("[\\r\\n]+", " ") + "\n"
+        }.mkString
+    }
+
+    def readId(text : String) = text.split("/") match {
+        case Array(userId, name) => Id(userId, name)
+        case _ => throw new RuntimeException("Can't parse ID: " + text)
+    }
+
+    def readBuildOrder(text : String) : BuildOrder = {
+        val entries = readEntries(text)
+        def expect[A](entry : Entry, keys : String*)(body : Seq[String] => A) = {
+            val map = entry.keyValues.toMap
+            val extra = map.keySet -- keys
+            if(extra.nonEmpty) throw new RuntimeException("Unexpected keys: " + extra.mkString(", "))
+            val values = keys.map(k => map.getOrElse(k, throw new RuntimeException("Expected key: " + k)))
+            body(values)
+        }
+        if(!entries.headOption.exists(_.text == "initial")) throw new RuntimeException("File must start with [initial]")
+        val initialAnimationId = expect(entries.head, "animationId") { case Seq(animationId) => readId(animationId) }
+        var actions = ListBuffer[Action]()
+        for(entry <- entries) entry match {
+            case Entry("effect", keyValues) => expect(entry, "factor", "effectId") { case Seq(factor, effectId) =>
+                actions += Effect(factor.toDouble, readId(effectId))
+            }
+            case Entry("combine", keyValues) => expect(entry, "animationId", "effectId", "flipped") { case Seq(animationId, effectId, flipped) =>
+                actions += Combine(readId(animationId), readId(effectId), flipped.toBoolean)
+            }
+            case Entry(name, _) => throw new RuntimeException("Unexpected [" + name + "]")
+        }
+        BuildOrder(initialAnimationId, actions)
     }
 
     def readEntries(text : String) : Seq[Entry] = {
