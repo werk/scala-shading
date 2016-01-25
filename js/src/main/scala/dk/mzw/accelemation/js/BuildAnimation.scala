5bb9ef97dbd7a3b2cd1344e715d78375c5297619
werk <mfwerk@gmail.com>
2016-01-25 07:55:23 +0100

    .

--- js/src/main/scala/dk/mzw/accelemation/js/BuildAnimation.scala
+++ js/src/main/scala/dk/mzw/accelemation/js/BuildAnimation.scala
@@ -1,9 +1,11 @@
 package dk.mzw.accelemation.js
 
+import dk.mzw.accelemation.Combinators._
 import dk.mzw.accelemation.Language._
-import dk.mzw.accelemation.ToGlsl
+import dk.mzw.accelemation.{Internal, ToGlsl}
 import dk.mzw.accelemation.js.BuildOrder._
 import dk.mzw.accelemation.js.debug.{LongTime, ExecutionTime}
+import dk.mzw.accelemation.js.widget.Scroller
 
 class BuildAnimation(
     var effectMap : Map[Id, R => Animation => Animation],
@@ -87,6 +89,15 @@ class BuildAnimation(
         //println(glsl)
         //println()
         glsl
+    }
+
+    def allList() : String = {
+        val functions = sortedAnimations.map(compiled(_).glsl).mkString
+        val animation = Scroller.tiled(compiled.values.map(_.call).toList)
+        val (glsl, time) = ExecutionTime(ToGlsl(animation, functions))
+        println(s"All list to GLSL: ${glsl.length / 1000} KB in ${LongTime.pretty(time)}")
+        glsl
+
     }
 
     private def resort(): Unit = {
--- js/src/main/scala/dk/mzw/accelemation/js/widget/ScrollListWidget.scala
+++ js/src/main/scala/dk/mzw/accelemation/js/widget/ScrollListWidget.scala
@@ -1,29 +1,52 @@
 package dk.mzw.accelemation.js.widget
 
-import dk.mzw.accelemation.Language.Animation
-import dk.mzw.accelemation.ToGlsl
-import dk.mzw.accelemation.js.ViewState.{Pick0, Pick1, Pick2, ShowList, ShowSave}
+import dk.mzw.accelemation.Internal
+import dk.mzw.accelemation.Language.{Vec2, R, Term}
 import dk.mzw.accelemation.js._
 import dk.mzw.accelemation.js.widget.Gui._
-import dk.mzw.accelemation.samples.{HidingDevils, TimeLens}
 import org.scalajs.dom
 import org.scalajs.dom.Element
 
 class ScrollListWidget(buildAnimation : BuildAnimation) extends Widget {
 
-    private val canvas = dom.document.createElement("canvas")
-    private val animade = new Animade(Animade.Configuration(ToGlsl(Scroller.scaled), canvas))
+    private val offset : Vec2 = Term(Internal.BuiltIn("u_offset"))
+
+    private val canvas = dom.document.createElement("canvas").asInstanceOf[dom.html.Element]
+    private val animade = new Animade(Animade.Configuration(buildAnimation.allList(), canvas))
     private val start = System.currentTimeMillis()
 
-    override def onResize(width: Int, height: Int): Unit = animade.resize(width, height)
+    private val tower = div("height" -> "400%")().toDom
+
+    var baseOffset : Double = 0
+    override def onResize(width: Int, height: Int): Unit = {
+        tower.style.height = (height * 2) + "px"
+        baseOffset = height.toDouble / width - 1
+        animade.resize(width, height)
+    }
 
     override def onDraw(): Unit = {
         val now = System.currentTimeMillis()
-        animade.draw(Map("u_time" -> List[Double]((now - start) / 1000.0)))
+        val t = (now - start)  / 1000.0
+        setParameters(t)
+        animade.draw(Map("u_time" -> List[Double](t)))
     }
 
     override val element: Element = {
-        div()(canvas).toDom
+        canvas.style.position = "absolute"
+        canvas.style.top = "0"
+        canvas.style.right = "0"
+        canvas.style.bottom = "0"
+        canvas.style.left = "0"
+        div(
+            "overflow-y" -> "scroll",
+            "position" -> "relative",
+            "height" -> "100%"
+        )(canvas, tower).toDom
+    }
+
+    private def setParameters(t : Double) {
+        val y = baseOffset
+        animade.set(Map("u_offset" -> List(0, y)))
     }
 
 }
